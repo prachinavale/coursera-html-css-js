994bb1379b22f3889886b90aa188b56113cbaa9c
prachinavale <47290991+prachinavale@users.noreply.github.com>
2020-09-01 13:21:52 +0530

    Add files via upload

---
+++ Module4-solution/script.js
@@ -0,0 +1,13 @@
+(function (window) {
+	var names = ["Yaakov","John","jen","Jason","Paul","Frank","Larry","Paula","Laura","jim"];
+
+	for (var i = 0; i < names.length; i++) {
+		var firstLetter = names[i].charAt(0);
+		if (firstLetter === "J" || firstLetter === "j") {
+			byeGreeter.speak(names[i]);
+		} 
+		else {
+			helloGreeter.speak(names[i]);
+		}
+	}
+})(window);
